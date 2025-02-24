a75d1e90339bac2841062c19fb8707c2afa5e199
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2025-02-24 12:44:51 +0700

    feat: adding block for /login when user is logged

--- app/routes/login/layout.tsx
+++ app/routes/login/layout.tsx
@@ -1,6 +1,20 @@
-import { Outlet } from "react-router";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Outlet, useNavigate } from "react-router";
+import type store from "~/store";
+import { UserType } from "~/types";
 
 export default function Layout() {
+	const me = useSelector(
+		(state: ReturnType<typeof store.getState>) => state.auth.user,
+	);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (me.authType === UserType.OK) navigate("/");
+	}, [me.authType, navigate]);
+
+	if (me.authType === UserType.OK) return <></>;
 	return (
 		<div
 			className="w-full md:h-full flex flex-col flex-1 gap-5 md:p-2.5 overflow-hidden md:items-start items-center justify-center p-4 md:px-96"
