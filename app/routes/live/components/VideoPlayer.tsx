71b09e9b214ae2283ace8ced0ad016bfc933ee91
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2026-02-23 19:09:29 +0700

    feat: low latency

--- app/routes/live/components/VideoPlayer.tsx
+++ app/routes/live/components/VideoPlayer.tsx
@@ -164,9 +164,14 @@ export default function VideoPlayer({
 			const hls = new Hls({
 				liveSyncDurationCount: 3,
 				liveMaxLatencyDurationCount: 5,
+				lowLatencyMode: true
 			});
 			hls.loadSource(url ?? "");
 			hls.attachMedia(ref.current);
+			hls.on(Hls.Events.MEDIA_ATTACHED, () => {
+				hls.loadSource(url ?? "");
+			});
+
 			return () => {
 				hls.destroy();
 			};
