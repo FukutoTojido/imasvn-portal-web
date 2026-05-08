314a4b5682271737661ca42b012732a60d6ff6dd
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2026-05-09 04:13:20 +0700

    fix: remove unneccessary imports

--- app/routes/admin/live/haxx.tsx
+++ app/routes/admin/live/haxx.tsx
@@ -14,7 +14,6 @@ import {
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 import { Textarea } from "~/components/ui/textarea";
-import type { Route } from "./+types/preview";
 
 export default function Page() {
 	const [submitting, setSubmitting] = useState(false);
