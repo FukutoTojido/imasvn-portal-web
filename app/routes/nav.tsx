a6b3dbc06035e3c6751ee18d2e935a7ca1e3e39d
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2026-07-21 13:04:25 +0700

    fix: scrollbar

--- app/routes/nav.tsx
+++ app/routes/nav.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router";
-import NavBar from "./components/NavBar";
 import ImageViewer from "./components/ImageViewer";
+import NavBar from "./components/NavBar";
 
 export default function Layout() {
 	return (
