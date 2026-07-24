3ef4b285760c29d5a8518874d05a2372ace66bc8
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2026-07-24 16:45:13 +0700

    fix: separate chat not working

--- app/routes/live/chat/page.tsx
+++ app/routes/live/chat/page.tsx
@@ -1,5 +1,5 @@
 import Chat from "../components/Chat";
 
 export default function Page() {
-    return <Chat isFullscreen={true} setViewers={() => {}} />;
+	return <Chat isFullscreen={true} setViewers={() => {}} isLive={true}/>;
 }
